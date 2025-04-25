8d2638a7681e5023e2f33a339d39099bee6c9787
Awadhesh08 <awadheshnirmalkar788@gmail.com>
2025-04-25 12:38:33 +0530

    some changes on github link on footer

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home/Home'
-import About from './components/About/about'
+import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import User from './components/User/User'
 import Github, { githubInfoLoader } from './components/Github/Github'
@@ -36,15 +36,15 @@ import Github, { githubInfoLoader } from './components/Github/Github'
 //Second Method
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:userid' element={<User/>}/>
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github/>}/>
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />} />
     </Route>
   )
 )
